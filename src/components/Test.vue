4fbbe07b47e881481463034773a886a9d29acf32
Pravinlkr <pravinlakhisarai@gmail.com>
2021-07-26 13:22:11 +0530

    added a select color dropdown, which will change color of text according to option selected

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,11 +1,20 @@
 <template>
-    <p :style="changeColorOfText()">Hello {{name}}</p><br/>
+    <p :style="selected">Hello {{name}}</p><br/>
     <p>{{currentTimee()}}</p><br/>
     <input type="checkbox" v-model="checkboxchecked" id="changeColor" value="checkedBox"/>
     <label for="changeColor">Change Color</label><br/><br/>
     <input v-model="name">
     <button v-on:click="clearName()">clear</button>
     <button v-on:click="changeToUpperCase()">Caps On</button>
+    <br/><br/>
+    <label for="colorDropDown">Choose a color</label>
+    <select v-model="selected" id="colorDropDown">
+        <!-- inline object literal -->
+        <option v-bind:value="{ color: 'red' }">Red</option>
+        <option v-bind:value="{ color: 'blue' }">Blue</option>
+        <option v-bind:value="{ color: 'yellow' }">Yellow</option>
+        <option v-bind:value="{ color: 'black' }">Black</option>
+    </select>
 </template>
 <script>
     export default{
@@ -15,6 +24,7 @@
                 name:"",
                 currentTime:"",
                 checkboxchecked:"",
+                selected: '',
                 redtext:{
                     color: 'red'
                 },
